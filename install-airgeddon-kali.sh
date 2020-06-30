852eccb88f6712119b2f00390c1cefba9fd49021
mareksagan <marek.sagan@protonmail.com>
2020-06-30 13:11:47 +0200

    Initial commit of the scripts

---
+++ install-airgeddon-kali.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/v1s1t0r1sh3r3/airgeddon/blob/master/binaries/kali/airgeddon_10.21-1_all.deb
+dpkg -i airgeddon_10.21-1_all.deb
---
+++ install-aliyun-hashcat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo yum update
+sudo yum upgrade
+sudo yum install clinfo unzip p7zip
+sudo yum install build-essential linux-headers-$(uname -r)
+sudo yum install -yq python3-pip 
+pip3 install psutil
+wget https://hashcat.net/files/hashcat-6.0.0.7z
+wget https://github.com/hashcat/hashcat-utils/releases/download/v1.9/hashcat-utils-1.9.7z
+p7zip x hashcat-6.0.0.7zip
+p7zip x hashcat-utils-1.9.7z
+screen -S hashcat ./hashcat-6.0.0/hashcat.bin -m 16800 pmkid.txt -a 3 -w 3 '?u?u?u?u?u?u?u?u'
